70dfccfd63d0c12d0673618da203760119843b1e
Gabriel Lisboa <devopup08@gmail.com>
2026-03-17 22:23:25 -0300

    ola

---
+++ login.py
@@ -0,0 +1,8 @@
+senha = "123456"
+resposta = int 
+
+resposta(int(input("Digite a senha: "))) == senha
+if senha == (senha):
+    print("Acesso permitido")
+else:
+    print("Acesso negado")
---
+++ loop.py
@@ -0,0 +1,6 @@
+import time
+
+
+for i in range(5):
+    time.sleep(0.5)
+    print("Eu sou programador!")
