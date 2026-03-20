e0b848643eb3ad8151fea037d9b5479ab0e2a9b9
Gabriel Lisboa <devopup08@gmail.com>
2026-03-20 10:32:14 -0300

    melhored

--- advinhe.py
+++ advinhe.py
@@ -1,3 +1,8 @@
+
+
+print("Bem vindo ao Jogo de Descobrir números.")
+print("O computador escolheu um número de 1 a 10. Tente advinhar!")
+
 # import 
 
 import random
@@ -6,15 +11,18 @@ import random
 
 numero_secreto = random.randint(1, 10)
 tentativa = 0
+tentativas_contador = 0
+limite = 5
 
 # codigo
 
-while tentativa != numero_secreto:
-    tentativa = int(input("Tente descobrir o número de 1 a 10."))
+while tentativa != numero_secreto and tentativas_contador < limite:
+    tentativa = int(input("Tente descobrir o número de 1 a 10: "))
+    tentativas_contador += 1
 
     if tentativa < numero_secreto:
         print("número mais baixo do que o que eu escolhi")
     elif tentativa > numero_secreto:
         print("número é maior do que o que eu escolhi")
     else:
-        print("acertou")
+        print(f"acertou, voce precisou de {tentativas_contador} tentativas.  ")
